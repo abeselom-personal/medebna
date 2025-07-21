9edfd8193fefd1b5be2c5333efa7849c0d6e4a9f
abeselom-personal <abeselomsolomongetahun@gmail.com>
2025-07-21 10:30:33 +0000

    FIX: public dto

--- controller/public/room.controller.js
+++ controller/public/room.controller.js
@@ -1,12 +1,13 @@
 import { getAllRoomsService, getRoomByIdService } from '../../services/public/public.service.js'
 import { RoomDTO, PaginatedDTO } from '../../dtos/public.dto.js'
+import { roomResponseDto, roomCreateDto, roomUpdateDto } from '../../dtos/room.dto.js'
 import mongoose from 'mongoose'
 
 export const getAllRooms = async (req, res) => {
     const page = +req.query.page || 1
     const limit = +req.query.limit || 10
     const [rooms, total] = await getAllRoomsService({ page, limit })
-    res.status(200).json(PaginatedDTO(rooms.map(RoomDTO), page, limit, total))
+    res.status(200).json(PaginatedDTO(rooms.map(roomResponseDto), page, limit, total))
 }
 
 export const getRoomById = async (req, res) => {
@@ -19,5 +20,5 @@ export const getRoomById = async (req, res) => {
     const room = await getRoomByIdService(id)
     console.log(room)
     if (!room) return res.status(404).json({ message: 'Room not found' })
-    res.status(200).json(RoomDTO(room))
+    res.status(200).json(roomResponseDto(room))
 }
