1022d52b8eb512eecbef3b66dfcb9ab21320ec20
abeselom-personal <abeselomsolomongetahun@gmail.com>
2025-07-27 09:44:05 +0000

    fixed role implementation

--- utils/progress.util.js
+++ utils/progress.util.js
@@ -2,6 +2,7 @@
 export const calculateProgress = (stepsCompleted) => {
     const steps = stepsCompleted.toObject ? stepsCompleted.toObject() : { ...stepsCompleted };
     delete steps._id;
+    delete steps.rooms;
     const done = Object.values(steps).filter(v => v === true).length;
     const total = Object.keys(steps).length;
     return Math.round((done / total) * 100);
